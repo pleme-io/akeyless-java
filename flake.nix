0b19901982e48ba50c5944af9d1919528de0735b
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:32 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,18 @@
 {
   description = "Akeyless Java SDK";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    substrate = {
-      url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkJavaMavenPackage = (import "${substrate}/lib/java-maven.nix").mkJavaMavenPackage;
-    in {
-      packages.default = mkJavaMavenPackage pkgs {
-        pname = "akeyless-java";
-        version = "0.0.0-dev";
-        src = self;
-        mvnHash = ""; # requires nix build to compute
-        description = "Akeyless Java SDK";
-        homepage = "https://github.com/pleme-io/akeyless-java";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ jdk17 maven ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "java";
+    builder = "package";
+    pname = "akeyless-java";
+    mvnHash = "";
+    description = "Akeyless Java SDK";
+  };
 }
